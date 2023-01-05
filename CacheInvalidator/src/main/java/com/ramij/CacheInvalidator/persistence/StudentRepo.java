f816abf6eeb66f2e80712ada1db3ae5c1081c408
Ramij <ramijnalpur@gmail.com>
2023-01-05 20:32:05 +0530

    Fixing findByReg Api

--- CacheInvalidator/src/main/java/com/ramij/CacheInvalidator/persistence/StudentRepo.java
+++ CacheInvalidator/src/main/java/com/ramij/CacheInvalidator/persistence/StudentRepo.java
@@ -2,12 +2,12 @@ package com.ramij.CacheInvalidator.persistence;
 
 import com.ramij.CacheInvalidator.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student,Long> {
-    public default  Student getStudentByRegNo(String regNo){
-        return new Student();
-    }
+    @Query("SELECT s FROM Student s WHERE s.regNo=?1")
+    Student getStudentByRegNo(String regNo);
 
 }
