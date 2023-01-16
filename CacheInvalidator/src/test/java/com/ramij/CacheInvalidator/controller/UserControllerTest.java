78aa88f9b7687487d5251dcb97d8897f605d3d13
Ramij <ramijnalpur@gmail.com>
2023-01-16 23:35:33 +0530

    Adding Tesdt Case For Update Student

--- CacheInvalidator/src/test/java/com/ramij/CacheInvalidator/controller/UserControllerTest.java
+++ CacheInvalidator/src/test/java/com/ramij/CacheInvalidator/controller/UserControllerTest.java
@@ -1,44 +1,78 @@
 package com.ramij.CacheInvalidator.controller;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ramij.CacheInvalidator.model.Student;
-import com.ramij.CacheInvalidator.service.StudentService;
-import org.hamcrest.Matchers;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@WebMvcTest(UserController.class)
+import java.io.IOException;
+
+@SpringBootTest
+//@WebMvcTest(UserController.class)
 @RunWith(SpringJUnit4ClassRunner.class)
+@AutoConfigureMockMvc
 public class UserControllerTest {
-    @MockBean
-    StudentService studentService;
     @Autowired
     MockMvc mockMvc;
     @Autowired
     ObjectMapper mapper;
 
+    private String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+
+    private <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
+
+//    @Test
+//    public void getStudentById() throws Exception {
+//        String studentJson = "    {\n" +
+//                "        \"id\": 5,\n" +
+//                "        \"name\": \"ramij roy\",\n" +
+//                "        \"email\": \"ramijnalpeerr@gmail.com\",\n" +
+//                "        \"course\": \"CHM\",\n" +
+//                "        \"regNo\": \"bc48e415-d448-46cb-98be-96dbc9f5ea89\"\n" +
+//                "    }";
+//
+//        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/student/5"))
+//                .andReturn();
+//        Student s = mapFromJson(studentJson,Student.class);
+//        JSONAssert.assertEquals(result.getResponse().getContentAsString(), mapper.writeValueAsString(s), false);
+//    }
+
     @Test
-    public void getStudentById() throws Exception {
-        Student s=new Student();
-        s.setRegNo("abcd");
+    public void createStudent() throws Exception {
+        Student s = new Student();
         s.setName("ramij");
         s.setCourse("CSE");
         s.setEmail("abc@gmail.com");
-        s.setId(1L);
-        Mockito.when(studentService.getStudentById(s.getId())).thenReturn(s);
-        MvcResult result= mockMvc.perform(MockMvcRequestBuilders.get("/api/student/1"))
+        String inputJson = mapToJson(s);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/student/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(inputJson)
+                )
                 .andReturn();
-        result.getResponse();
+        String str = result.getResponse().getContentAsString();
+        JSONAssert.assertNotEquals(result.getResponse().getContentAsString(), new JSONObject(), false);
     }
+
 }
